74b37e3c9ce4e11fa2d1cd1f396348d6cffb7c50
Bruce <zzzwco@outlook.com>
2022-12-01 09:41:28 +0800

    Add Hud style

--- Sources/Aghs/UI/Hud.swift
+++ Sources/Aghs/UI/Hud.swift
@@ -37,28 +37,27 @@ public extension Ax where T: View {
 
 public final class Hud: ObservableObject {
   @Published public var isPresented = false
-  var content: AnyView = AnyView(EmptyView())
-  var background: AnyView = AnyView(Color.black.opacity(0.6))
-  var presentationMode: PresentationMode = .forever()
+  var content: any View = EmptyView()
+  var style: HudStyle = .default()
   
   public init() {}
   
   public func show(
-    background: some View = AnyView(Color.black.opacity(0.6)),
-    presentationMode: PresentationMode = .forever(),
+    style: HudStyle = .default(),
+    animation: Animation? = .spring(),
     content: () -> some View
   ) {
-    self.background = AnyView(background)
-    self.presentationMode = presentationMode
-    self.content = AnyView(content())
-    withAnimation {
+    self.style = style
+    self.content = content()
+    withAnimation(animation) {
       isPresented = true
     }
   }
   
-  public enum PresentationMode {
-    case forever(interactiveHide: Bool = true)
-    case dimissAfter(seconds: Double = 2.0)
+  public func hide(_ animation: Animation? = .spring()) {
+    withAnimation(animation) {
+      isPresented = false
+    }
   }
 }
 
@@ -68,22 +67,125 @@ public extension Aghs.Bag {
     @StateObject public var hud: Hud
     
     public func body(content: Content) -> some View {
-      ZStack {
+      ZStack(alignment: hud.style.position.alignment) {
         content
         
         if hud.isPresented {
-          hud.background
-            .ignoresSafeArea()
-            .transition(.opacity)
-            .onTapGesture {
-              hud.isPresented = false
-            }
-          
-          hud.content
-            .transition(.opacity.combined(with: .scale))
+          Group {
+            AnyView(hud.style.background)
+              .ignoresSafeArea()
+              .transition(.opacity)
+              .onTapGesture {
+                if hud.style.interactiveHide {
+                  hud.hide()
+                }
+              }
+              .onAppear {
+                if let duration = hud.style.duration {
+                  DispatchQueue.main.asyncAfter(
+                    deadline: .now() + duration) {
+                      hud.hide()
+                    }
+                }
+              }
+            
+            AnyView(hud.content)
+              .transition(.opacity.combined(with: .scale))
+          }
+          .zIndex(.infinity)
         }
       }
       .environmentObject(hud)
     }
+  }
+}
+
+public protocol HudStyle {
+  var background: any View { get set }
+  var interactiveHide: Bool { get set }
+  var duration: Double? { get set }
+  var position: Aghs.Bag.HudContentPosition { get set }
+}
+
+public extension HudStyle where Self == Aghs.Bag.DefaultHudStyle {
+  
+  static func `default`(
+    background: any View = Color.black.opacity(0.6),
+    interactiveHide: Bool = true,
+    duration: Double? = nil,
+    position: Aghs.Bag.HudContentPosition = .center
+  ) -> Aghs.Bag.DefaultHudStyle {
+    .init(
+      background: background,
+      interactiveHide: interactiveHide,
+      duration: duration,
+      position: position
+    )
+  }
+}
+
+public extension HudStyle where Self == Aghs.Bag.TipHudStyle {
+  
+  static func tip(
+    style: Aghs.Bag.TipStyle = .default(),
+    duration: Double? = 2.0,
+    position: Aghs.Bag.HudContentPosition = .center
+  ) -> Aghs.Bag.TipHudStyle {
+    var background: any View = Color.clear
+    var interactiveHide = true
+    var duration = duration
+    switch style {
+    case .default(let value):
+      background = value ?? Color.clear
+    case .loading(let value):
+      background = value ?? Color.black.opacity(0.0001)
+      interactiveHide = false
+      duration = nil
+    }
+    return .init(
+      background: background,
+      interactiveHide: interactiveHide,
+      duration: duration,
+      position: position
+    )
+  }
+}
+
+public extension Aghs.Bag {
+  
+  enum HudContentPosition {
+    case top
+    case center
+    case bottom
+    
+    var alignment: Alignment {
+      switch self {
+      case .top:
+        return .top
+      case .center:
+        return .center
+      case .bottom:
+        return .bottom
+      }
+    }
+  }
+  
+  struct DefaultHudStyle: HudStyle {
+    public var background: any View
+    public var interactiveHide: Bool
+    public var duration: Double?
+    public var position: HudContentPosition
+  }
+  
+  struct TipHudStyle: HudStyle {
+    public var background: any View
+    public var interactiveHide: Bool
+    public var duration: Double?
+    public var position: HudContentPosition
+  }
+  
+  enum TipStyle {
+    case `default`(background: (any View)? = nil)
+    case loading(background: (any View)? = nil)
   }
 }
