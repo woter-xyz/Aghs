f79aaa4f999771fdf0a2a41fc1d012f8ed1d368e
zzzwco <zzzwco@outlook.com>
2023-04-20 23:34:56 +0800

    Update

--- Package.swift
+++ Package.swift
@@ -25,9 +25,7 @@ let package = Package(
     // Targets can depend on other targets in this package, and on products in packages this package depends on.
     .target(
       name: "Aghs",
-      resources: [
-        .process("Resources/")
-      ]),
+      dependencies: []),
     .testTarget(
       name: "AghsTests",
       dependencies: ["Aghs"]),
