f95ccbebf3daa554d5d83c90c9552732293166ae
zzzwco <zzzwco@outlook.com>
2023-05-14 22:59:27 +0800

    Update Hud

--- Sources/Aghs/ViewModifiers/Hud.swift
+++ Sources/Aghs/ViewModifiers/Hud.swift
@@ -28,14 +28,50 @@
 import Foundation
 import SwiftUI
 
-extension Ax where T: View {
+public extension Ax where T: View {
   
   /// Attach a Hud to the view.
   ///
   /// - Parameter hud: A `Hud` instance to be attached to the view.
   /// - Returns: The original view with the Hud modifier applied.
-  public func hud(_ hud: Hud) -> some View {
-    base.modifier(HudModifier(hud: hud))
+  func initHud(_ hud: Hud, defaultStyle: HudStyle = Hud.defaultStyle) -> some View {
+    Hud.defaultStyle = defaultStyle
+    return base.modifier(HudModifier(hud: hud))
+  }
+  
+  func hud(
+    isPresented: Binding<Bool>,
+    style: HudStyle = Hud.defaultStyle,
+    @ViewBuilder content: @escaping () -> some View
+  ) -> some View {
+    base.modifier(
+      HudViewModifier(
+        isPresented: isPresented,
+        style: style
+      ) {
+        content()
+      }
+    )
+  }
+}
+
+public struct HudViewModifier<C: View>: ViewModifier {
+  @Binding var isPresented: Bool
+  var style: HudStyle
+  @ViewBuilder var hudView: () -> C
+  @EnvironmentObject var hud: Hud
+  
+  public func body(content: Content) -> some View {
+    content
+      .onChange(of: isPresented) { newValue in
+        if newValue {
+          hud.show(style: style) {
+            hudView()
+          }
+        } else {
+          hud.hide()
+        }
+      }
   }
 }
 
@@ -50,53 +86,31 @@ extension Ax where T: View {
 ///
 /// Then you can use hud in any view of the app:
 ///
-@MainActor
 public final class Hud: ObservableObject {
   @Published public var isPresented = false
-  var content: any View = EmptyView()
-  var style: HudStyle = .default()
+  public static var defaultStyle: HudStyle = .default
   
-  public static var defaultStyle: HudStyle = .default()
+  var content: AnyView = AnyView(EmptyView())
+  var style: HudStyle = .default
   
   public init() {}
   
-  /// Display the Hud with the specified style, animation, and content.
-  ///
-  /// - Parameters:
-  ///   - style: A `HudStyle` instance specifying the appearance and behavior of the Hud.
-  ///   - animation: The animation to use when showing the Hud.
-  ///   - content: A closure that returns the content of the Hud.
   public func show(
-    style: HudStyle,
-    animation: Animation? = .spring(),
-    content: () -> some View
+    style: HudStyle = Hud.defaultStyle,
+    @ViewBuilder content: () -> some View
   ) {
     self.style = style
-    self.content = content()
-    withAnimation(animation) {
+    self.content = AnyView(content())
+    withAnimation(.default) {
       isPresented = true
     }
-  }
-  
-  /// Display the Hud using the global default style, a specified animation, and content.
-  ///
-  /// This method uses the global `defaultStyle` as the Hud style.
-  ///
-  /// - Parameters:
-  ///   - animation: The animation to use when showing the Hud. Defaults to `.spring()`.
-  ///   - content: A closure that returns the content of the Hud.
-  public func show(
-    animation: Animation? = .spring(),
-    content: () -> some View
-  ) {
-    show(style: Self.defaultStyle, animation: animation, content: content)
   }
   
   /// Hide the Hud with the specified animation.
   ///
   /// - Parameter animation: The animation to use when hiding the Hud.
-  public func hide(_ animation: Animation? = .spring()) {
-    withAnimation(animation) {
+  public func hide() {
+    withAnimation(.default) {
       isPresented = false
     }
   }
@@ -107,70 +121,84 @@ public struct HudModifier: ViewModifier {
   @StateObject public var hud: Hud
   
   public func body(content: Content) -> some View {
-    ZStack(alignment: hud.style.alignment) {
-      content
-      
-      if hud.isPresented {
-        Group {
-          AnyView(hud.style.background)
-            .ignoresSafeArea()
-            .transition(.opacity)
-            .onTapGesture {
-              if hud.style.interactiveHide {
-                hud.hide()
-              }
+    content
+      .overlay {
+        hud.style.background
+          .ignoresSafeArea()
+          .onTapGesture {
+            if hud.style.interactiveHide {
+              hud.hide()
             }
-            .onAppear {
-              if let duration = hud.style.duration {
-                DispatchQueue.main.asyncAfter(
-                  deadline: .now() + duration) {
-                    hud.hide()
-                  }
-              }
+          }
+          .opacity(hud.isPresented ? 1 : 0)
+          .overlay(alignment: hud.style.alignment) {
+            if hud.isPresented {
+              hud.content
+                .transition(hud.style.transition)
             }
-          
-          AnyView(hud.content)
-            .transition(hud.style.transiton)
-        }
-        .zIndex(.infinity)
+          }
       }
-    }
-    .environmentObject(hud)
-    .ignoresSafeArea()
+      .environmentObject(hud)
   }
 }
 
 public protocol HudStyle {
-  var background: any View { get set }
-  var interactiveHide: Bool { get set }
-  var alignment: Alignment { get set }
-  var transiton: AnyTransition { get set }
-  var duration: Double? { get set }
+  var background: AnyView { get }
+  var interactiveHide: Bool { get }
+  var alignment: Alignment { get }
+  var transition: AnyTransition { get }
 }
+
+public extension HudStyle {
+  var background: AnyView { AnyView(Color.black.opacity(0.6)) }
+  var interactiveHide: Bool { true }
+  var alignment: Alignment { .center }
+  var transition: AnyTransition { .opacity.combined(with: .scale) }
+}
+
+public struct DefaultHudStyle: HudStyle {}
 
 extension HudStyle where Self == DefaultHudStyle {
   
-  public static func `default`(
-    background: any View = Color.black.opacity(0.6),
-    interactiveHide: Bool = true,
-    alignment: Alignment = .center,
-    transiton: AnyTransition = .opacity.combined(with: .scale),
-    duration: Double? = nil
-  ) -> DefaultHudStyle {
+  public static var `default`: DefaultHudStyle {
+    .init()
+  }
+}
+
+public struct CustomHudStyle: HudStyle {
+  public var background: AnyView
+  public var interactiveHide: Bool
+  public var alignment: Alignment
+  public var transition: AnyTransition
+  
+  public init(
+    background: AnyView,
+    interactiveHide: Bool,
+    alignment: Alignment,
+    transition: AnyTransition
+  ) {
+    self.background = background
+    self.interactiveHide = interactiveHide
+    self.alignment = alignment
+    self.transition = transition
+  }
+}
+
+extension HudStyle where Self == CustomHudStyle {
+  
+  public static func custom(
+    background: AnyView,
+    interactiveHide: Bool,
+    alignment: Alignment,
+    transition: AnyTransition
+  ) -> CustomHudStyle {
     .init(
       background: background,
       interactiveHide: interactiveHide,
       alignment: alignment,
-      transiton: transiton,
-      duration: duration
+      transition: transition
     )
   }
 }
 
-public struct DefaultHudStyle: HudStyle {
-  public var background: any View
-  public var interactiveHide: Bool
-  public var alignment: Alignment
-  public var transiton: AnyTransition
-  public var duration: Double?
-}
+
